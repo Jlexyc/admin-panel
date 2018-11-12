cbcdbc31772241e4a463ae65f8df509dfe40faf9
Oleksii Kozulin <senior.jlexyc@gmail.com>
2018-11-12 13:29:51 +0200

    Fixed coma

--- config/default.env.js
+++ config/default.env.js
@@ -3,7 +3,7 @@ module.exports = {
   HORIZON_SERVER: '"http://94.130.199.64:8000"',
   STORAGE_SERVER: '"http://94.130.199.64:9001/api"',
   KEY_SERVER_ADMIN: '"http://94.130.199.64:8006"',
-  NETWORK_PASSPHRASE: '"TokenD Developer Network"'
+  NETWORK_PASSPHRASE: '"TokenD Developer Network"',
   FEATURES: {
     /* Enable authentication using seed. Such auth has no 2FA.
     * WARN: for demo purposes only, could be very dangerous on prod
